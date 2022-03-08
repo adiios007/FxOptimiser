e96ec71c6a65f6ebd72196d6273c4defa530a9f1
adiios007 <dth.bhat@gmail.com>
2022-03-08 16:02:46 +0530

    made few changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const orderRoutes = require('./routes/orderdetails');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.use('/fx', homeRoutes);
 app.use('/currencyconvertor', xchangeRoutes);
